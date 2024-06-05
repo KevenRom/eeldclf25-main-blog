35c6cde69bd4380896cf32ff243a8f7222b8b410
Seong Jong Kim <eeldclf25@gmail.com>
2024-06-05 22:59:41 +0900

    first setup

---
+++ src/test/java/com/eeldclf25/main_blog/MainBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeldclf25.main_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
